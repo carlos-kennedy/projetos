d309acdda58b7bc503d1f4dbf3b74f1793bd56ff
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-06-22 19:50:35 -0300

    Update, desafio completo!

--- rocketblog/main.js
+++ rocketblog/main.js
@@ -5,3 +5,21 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove("menuExpanded");
 }
+
+ScrollReveal({
+  origin: "right",
+  distance: "4rem",
+  duration: 900,
+}).reveal(`#home`);
+
+ScrollReveal({
+  origin: "top",
+  distance: "4rem",
+  duration: 900,
+}).reveal(`#posts`);
+
+ScrollReveal({
+  origin: "bottom",
+  distance: "4rem",
+  duration: 1000,
+}).reveal(`#subPosts`);
